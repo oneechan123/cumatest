e6bfd38cf8ea43ccf3947f95b73c657d211458d1
oneechan123 <73242335+oneechan123@users.noreply.github.com>
2022-03-12 13:35:24 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,43 +1,48 @@
 let fs = require('fs')
-global.owner = ['628112958665', '6288215569001','628998512588', '6283175998566', '6285693602003'] // Letakan nomor kamu disini
+global.owner = ['6285693602003'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
+  hardianto: 'https://hardianto-chan.herokuapp.com',
   jonaz: 'https://jonaz-api-v2.herokuapp.com',
-  neoxr: 'https://api.neoxr.eu.org/',
+  neoxr: 'https://neoxr-api.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
   pencarikode: 'https://pencarikode.xyz',
   xteam: 'https://api.xteam.xyz',
   zahir: 'https://zahirr-web.herokuapp.com',
   zekais: 'http://zekais-api.herokuapp.com',
-  zeks: 'https://api.zeks.me',
+  zeks: 'https://api.zeks.xyz',
   vhtear: 'https://api.vhtear.com',
   lolhum: 'https://api.lolhuman.xyz',
+  fxc7: 'https://fxc7-api.herokuapp.com',
   bx: 'https://bx-hunter.herokuapp.com',
 
 }
 global.APIKeys = { // APIKey nya disini
   // 'https://website': 'apikey'
-  'https://api.neoxr.eu.org/': 'yntkts',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'APIKEY',
-  'https://api.xteam.xyz': 'HIRO',
+  'https://api.xteam.xyz': 'cffb066da6c4f0c9',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.me': 'apivinz',
-  'https://api.lolhuman.xyz': '929b48b9aa5a91abed8526cd',
+  'https://api.zeks.xyz': 'FVe0JFCdME58YSKmhoDWdRtyuJt',
+  'https://api.lolhuman.xyz': 'YoshinoKhoirul',
   'https://api.vhtear.com': 'sayahafiz',
+  'https://fxc7-api.herokuapp.com': 'Fxc7',
+  'https://api.justaqul.xyz': '5kbUqdG00OXelFYx',
   'http://zekais-api.herokuapp.com': 'grqgD6pU',
   'https://bx-hunter.herokuapp.com': 'Ikyy69',
 }
 
 // Sticker WM
-global.packname = 'Haruno'
-global.author = 'Fadli'
+global.packname = 'Yoshino'
+global.author = '+62856935963120'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© Haruno'
-global.thumbfoto = 'https://telegra.ph/file/2249d5c5fa9785d2682aa.jpg' 
-global.image = 'https://telegra.ph/file/39bbded9693c9338069fd.jpg'
+global.watermark = '© Yoshino\nKalo ada yg eror langsung #report ya supaya langsung dibenerin sama owner' //change the watermark 
+global.image = 'https://telegra.ph/file/8ff28bdf1ee84e756db24.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/3b2c7554ca0ed325d161b.jpg'
 
 global.multiplier = 59 // Semakin tinggi, semakin sulit naik level
 
